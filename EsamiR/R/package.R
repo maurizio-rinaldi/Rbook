27439e7111560106079fc4361ca33a45ebdebf65
Maurizio Rinaldi <mauriziorinaldi@maurizios-MacBook-Pro-New.local>
2016-02-01 12:46:22 +0100

    variazioni non pertinenti
    p

--- EsamiR/R/package.R
+++
@@ -1,7 +0,0 @@
-#' Skeleton of R package.
-#' @name TEST
-#' @docType package
-#' @examples
-#' f <- Funzione()
-#' @import methods
-#' @import graphics
--- EsamiR/R/plotMethod.R
+++
@@ -1,9 +0,0 @@
-#' @include funzioneClass.R
-setMethod(f = 'plot',
-          signature= 'Funzione',
-          definition = function (x,y,...)
-                 { g<-x@fR
-                 d<-x@dominio
-          curve(g,d[1],d[2])
-          }
-)
