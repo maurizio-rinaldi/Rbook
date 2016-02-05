718bf8460d896e6d830b834c2bd11e552b005bf9
Maurizio Rinaldi <mauriziorinaldi@maurizios-MacBook-Pro-New.local>
2016-02-05 08:48:28 +0100

    5 feb Dati libro

---
+++ LibroR/R/data.R
@@ -0,0 +1,115 @@
+#' Mortalita' dei moscerini  su un periodo di 173 giorni
+#'
+#' Un data set di sopravvivenza di una colonia di moscerini
+#'
+#' @format Un data frame di 173 righe e 3 variabili:
+#' \describe{
+#'   \item{day }{tempo in giorni}
+#'   \item{living}{moscerini vivi}
+#'   \item{mort.rate}{tasso di mortalita' per ogni giorno}
+#'   ...
+#' }
+#' @source \url{http://lib.stat.cmu.edu/DASL/Datafiles/Medflies.html}
+"gompertz"
+
+# Crabs: Misure morfologiche di 200 granchi Leptograpsus 
+#' @format Un data frame di 200 righe e 8 variabili:
+#' \describe{
+#'   \item{sp }{specie}
+#'   \item{sex}{moscerini vivi}
+#'   \item{index}{tasso di mortalita' per ogni giorno}
+#'   \item{FL}{tasso di mortalita' per ogni giorno}
+#'   \item{RW}{tasso di mortalita' per ogni giorno}
+#'   \item{CW}{tasso di mortalita' per ogni giorno}
+#'   \item{BD}{tasso di mortalita' per ogni giorno}
+#' }
+#' @source \url{http://lib.stat.cmu.edu/DASL/Datafiles/Medflies.html}
+"crabs"
+
+#' Dati meteo  a Milano
+#' 
+#' @format Un data frame di 173 righe e 3 variabili:
+#' \describe{
+#'   \item{day }{tempo in giorni}
+#'   \item{living}{moscerini vivi}
+#'   \item{mort.rate}{tasso di mortalita' per ogni giorno}
+#'   ...
+#' }
+#' @source \url{http://lib.stat.cmu.edu/DASL/Datafiles/Medflies.html}
+"meteo"
+
+#' Interesse dei bambini
+#' 
+#' I dati: Un dataset che contiene informazioni su 478 bambini
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{Gender}{Sesso}
+#'   \item{Age}{eta' in anni}
+#'   \item{Race}{Bianco o altro}
+#'   \item{Urban/Rural}{distretto scolastico di tipo rurale, urbano o suburbano}
+#'   \item{School}{scuola}
+#'   \item{Goals}{Gli obiettivi degli studenti: 1 =Ottenere buone valutazioni, 2 = Essere popolari,
+#'    3 = Essere bravo negli sport}
+#'   \item{Grades}{(1=piu' importante per essere popolari, 4=meno importante)}
+#'   \item{Sport}{(1=piu' importante per essere popolari, 4=meno importante)}
+#'   \item{Looks}{(1=piu' importante per essere popolari, 4=meno importante)}
+#'   \item{Money}{(1=piu' importante per essere popolari, 4=meno importante)}
+#' }
+#' @source \url{http://lib.stat.cmu.edu/DASL/Datafiles/PopularKids.html}
+"kidinterest"
+
+
+
+#'I dati: Un dataset che contiene informazioni su 41 studenti di Biotec
+#'
+#' @format A data frame with 41 rows and 8 variabili:
+#' \describe{
+#'   \item{SESSO}{M/F}
+#'   \item{PESO}{peso in kg}
+#'   \item{ALTEZZA}{Altezza in metri}
+#'   \item{OCCHI}{Colore degli occhi}
+#'   \item{CAPELLI}{Colore dei capelli}
+#'   \item{SCARPE}{numero di scarpe}
+#'   \item{hMADRE}{Altezza della madre}
+#'   \item{hPADRE}{Altezza del padre}
+#'   
+#' }
+#' "biotec"
+#' 
+
+
+#'I dati: Un dataset che contiene informazioni su 55 studenti di Farmacia
+#'
+#' @format Un data frame con 41 righe e 8 variabili:
+#' \describe{
+#'   \item{SEX}{M/F}
+#'   \item{W}{peso in kg}
+#'   \item{H}{Altezza in metri}
+#'   \item{Eyes}{Colore degli occhi}
+#'   \item{Hair}{Colore dei capelli}
+#'   \item{Sh}{numero di scarpe}
+#'   \item{hM}{Altezza della madre}
+#'   \item{hF}{Altezza del padre}
+#' }
+#' 
+"farmacia"
+
+
+
+#'I dati: Un dataset che contiene informazioni su 96 studenti  
+#'
+#' @format Un data frame con 96 righe e 8 variabili:
+#' \describe{
+#'   \item{SEX}{M/F}
+#'   \item{W}{peso in kg}
+#'   \item{H}{Altezza in metri}
+#'   \item{Eyes}{Colore degli occhi}
+#'   \item{Hair}{Colore dei capelli}
+#'   \item{Sh}{numero di scarpe}
+#'   \item{hM}{Altezza della madre}
+#'   \item{hF}{Altezza del padre}
+#'   
+#' }
+#' 
+#' "studenti"
